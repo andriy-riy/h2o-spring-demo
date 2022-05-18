12beb2719469a7afe50928cc592223b415979aca
Andriy Riy <ariy@Andriys-MacBook-Pro-2.local>
2022-05-18 14:59:49 +0300

    Added pojo, mojo models using examples

--- src/main/java/com/rio/h2ospringdemo/service/PredictionService.java
+++ src/main/java/com/rio/h2ospringdemo/service/PredictionService.java
@@ -32,6 +32,9 @@ public class PredictionService {
             @Value("classpath:XGBoost_1_AutoML_2_20220518_143427.zip") Resource positionPredictionMojoModel,
             @Value("classpath:GBM_2_AutoML_3_20220518_144001.zip") Resource pricePredictionMojoModel) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
+        // POJO model: https://github.com/h2oai/h2o-3/blob/master/h2o-docs/src/product/pojo-quickstart.rst
+        // MOJO model: https://github.com/h2oai/h2o-3/blob/master/h2o-docs/src/product/mojo-quickstart.rst
+
         this.positionPredictionModel = MojoModel.load(positionPredictionMojoModel.getFile().getAbsolutePath());
         //this.positionPredictionModel = (GenModel) Class.forName("XGBoost_1_AutoML_2_20220518_143427").getDeclaredConstructor().newInstance();
 
